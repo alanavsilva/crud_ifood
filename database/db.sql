6a95365bd447f373cbd47597b8adaeab5bff6394
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-08-27 12:02:51 -0300

    Add- banco de dados

---
+++ database/db.sql
@@ -0,0 +1,30 @@
+create database crud_ifood;
+use crud_ifood;
+
+create table cliente (
+    id int auto_increment primary key,
+    nome varchar(100) not null,
+    email varchar(100) not null,
+    telefone varchar(100) not null,
+    endereco varchar(100) not null
+);
+
+create table restaurante (
+    id int auto_increment primary key, 
+    nome varchar(100) not null,
+    categoria varchar(100) not null,
+    telefone varchar(100) not null,
+    endereco varchar(100) not null
+);
+
+create table pedido (
+    id int auto_increment primary key,
+    data_pedido date not null, 
+    valor float not null,
+    status varchar(100) not null, 
+    id_cliente int not null,
+    id_restaurante int not null,
+
+    foreign key (id_cliente) references cliente(id),
+    foreign key (id_restaurante) references restaurante(id)
+)
